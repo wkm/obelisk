0ec8f0704474bdcdad1fc9d05d1eac88437b89f1
Wiktor Macura <wiktor@tumblr.com>
2013-04-20 16:52:13 -0400

    use a template when rendering errors, greedily reload templates

--- http/http.go
+++ http/http.go
@@ -57,14 +57,28 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Printf("err: %s", err.Error())
+		log.Printf("err: %s", err)
 	}
+}
+
+func getTemplates() *template.Template {
+	return buildTemplates("templates")
 }
 
 func error(rw http.ResponseWriter, msg string) {
 	rw.Header().Set("Status-Code", "502")
 	log.Printf("err: %s", msg)
-	fmt.Fprintf(rw, "Error: %s", msg)
+	err := getTemplates().ExecuteTemplate(rw, "error.html", msg)
+	if err != nil {
+		fmt.Fprintf(rw, "Could not render error template: %s", err.Error())
+	}
+}
+
+func renderTemplate(rw http.ResponseWriter, name string, object interface{}) {
+	err := getTemplates().ExecuteTemplate(rw, name, nil)
+	if err != nil {
+		error(rw, err.Error())
+	}
 }
 
 func indexHandler(rw http.ResponseWriter, req *http.Request) {
@@ -73,10 +87,8 @@ func indexHandler(rw http.ResponseWriter, req *http.Request) {
 	if path == "/" {
 		path = "/index.html"
 	}
-
-	var templates = buildTemplates("templates")
 	// try to find a template with the given name
-	err := templates.ExecuteTemplate(rw, path, nil)
+	err := getTemplates().ExecuteTemplate(rw, path, nil)
 	if err != nil {
 		log.Printf("err: %s", err.Error())
 		error(rw, err.Error())
