ac4c9762afccff309302725530299af7066a8607
Wiktor Macura <wiktor@tumblr.com>
2013-04-24 22:38:19 -0400

    simple facility for remote configuration

---
+++ rconfig/rconfig.go
@@ -0,0 +1,37 @@
+// remote configuration facility for workers
+package rconfig
+
+import (
+	"circuit/use/circuit"
+	"sync"
+)
+
+const ServiceName = "remote-config"
+
+// a simple configuration object
+type RConfig struct {
+	sync.Mutex
+	Settings map[string]string
+}
+
+var Config = new(RConfig)
+
+func init() {
+	circuit.RegisterValue(&RConfig{})
+}
+
+// change the value of a setting (threadsafe)
+func (r *RConfig) Set(key, value string) {
+	r.Lock()
+	defer r.Unlock()
+
+	r.Settings[key] = value
+}
+
+// get the value of a setting (threadsafe)
+func (r *RConfig) Get(key string) string {
+	r.Lock()
+	defer r.Unlock()
+
+	return r.Settings[key]
+}
