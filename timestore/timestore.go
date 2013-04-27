42ecce6f0bd43780a73842046e114f313f5bec5a
Wiktor Macura <wiktor@tumblr.com>
2013-04-26 21:18:10 -0400

    in-memory timestore implementation backed by an llrb

--- timestore/timestore.go
+++ timestore/timestore.go
@@ -1,12 +1,14 @@
 /*
 	dead simple in-memory store for timeseries, thread safe with a
-	global lock
+	global lock; flushes to disk on a regular basis
 */
 
 package timestore
 
 import (
+	"fmt"
 	"github.com/petar/GoLLRB/llrb"
+	"io"
 	"sync"
 )
 
@@ -31,11 +33,13 @@ func NewStore() *Store {
 
 // inserts the given time and value under the key
 func (s *Store) Insert(key, time uint64, value float64) *Store {
+	statInsert.Incr()
+
 	s.Lock()
 	defer s.Unlock()
 
 	// create a timeseries if we need to
-	if _, ok := s.values[key]; ok {
+	if _, ok := s.values[key]; !ok {
 		s.values[key] = newTree()
 	}
 
@@ -49,11 +53,15 @@ func newTree() *llrb.Tree {
 
 // return all points from key with time in [start,stop]
 func (s *Store) Query(key, start, stop uint64) ([]Point, error) {
+	statQuery.Incr()
+
 	s.Lock()
 	defer s.Unlock()
 
 	var ary []Point
 	iterfn := func(i llrb.Item) bool {
+		statIter.Incr()
+
 		p := i.(Point)
 		if p.Time <= stop {
 			ary = append(ary, p)
@@ -63,7 +71,7 @@ func (s *Store) Query(key, start, stop uint64) ([]Point, error) {
 	}
 
 	if ts, ok := s.values[key]; ok {
-		ts.AscendGreaterOrEqual(start, iterfn)
+		ts.AscendGreaterOrEqual(Point{start, 0}, iterfn)
 	}
 
 	return ary, nil
@@ -71,11 +79,15 @@ func (s *Store) Query(key, start, stop uint64) ([]Point, error) {
 
 // return all values from key with time [start,stop]
 func (s *Store) FlatQuery(key, start, stop uint64) ([]float64, error) {
+	statQuery.Incr()
+
 	s.Lock()
 	defer s.Unlock()
 
 	var ary []float64
 	iterfn := func(i llrb.Item) bool {
+		statIter.Incr()
+
 		p := i.(Point)
 		if p.Time <= stop {
 			ary = append(ary, p.Value)
@@ -85,13 +97,65 @@ func (s *Store) FlatQuery(key, start, stop uint64) ([]float64, error) {
 	}
 
 	if ts, ok := s.values[key]; ok {
-		ts.AscendGreaterOrEqual(start, iterfn)
+		ts.AscendGreaterOrEqual(Point{start, 0}, iterfn)
 	}
 
 	return ary, nil
 }
 
-// sorts Point by their time
+// write out all values into a writer, this blocks the entire time
+func (s *Store) Dump(w io.Writer) error {
+	statDump.Incr()
+
+	s.Lock()
+	defer s.Unlock()
+
+	var cnt int
+	var err error
+	hasErrored := false
+	iterfn := func(i llrb.Item) bool {
+		// don't iterate if we've errored
+		if hasErrored {
+			return false
+		}
+
+		p := i.(Point)
+		cnt, err = fmt.Fprintf(w, "%d=%f,", p.Time, p.Value)
+		statIter.Add(uint(cnt))
+		if err != nil {
+			statError.Incr()
+			hasErrored = true
+		}
+
+		return true
+	}
+
+	// iterate over all keys
+	for key, series := range s.values {
+		count, err := fmt.Fprintf(w, "%d:", key)
+		statDumpBytes.Add(uint(count))
+		if err != nil {
+			statError.Incr()
+			return err
+		}
+
+		series.AscendGreaterOrEqual(Point{0, 0}, iterfn)
+		if hasErrored {
+			return err
+		}
+
+		count, err = fmt.Fprintf(w, "\n")
+		statDumpBytes.Add(uint(count))
+		if err != nil {
+			statError.Incr()
+			return err
+		}
+	}
+
+	return nil
+}
+
+// sort Point by their time
 func lessPoint(a, b interface{}) bool {
 	return a.(Point).Time < b.(Point).Time
 }
--- timestore/timestore_test.go
+++ timestore/timestore_test.go
@@ -1,6 +1,7 @@
 package timestore
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -16,5 +17,12 @@ func TestStore(t *testing.T) {
 	}
 	if len(values) != 3 {
 		t.Errorf("expected 3 values, got %d", len(values))
+	}
+
+	var b bytes.Buffer
+	db.Dump(&b)
+
+	if b.String() != "123:10=1.100000,11=1.200000,12=1.300000,\n" {
+		t.Errorf("invalid dump %s", b.String())
 	}
 }
