4bcd3ffb72d1e923d955d96ecc8876717dd754bd
Wiktor Macura <wiktor@tumblr.com>
2013-05-06 21:33:47 -0400

    [rlog] working towards an flog service

--- lib/rlog/rlog.go
+++ lib/rlog/rlog.go
@@ -1,10 +1,20 @@
-// logging facility for workers, available remotely
+/*
+	logging facility for workers
+*/
 package rlog
 
 import (
 	"bytes"
 	"fmt"
 )
+
+type Log interface {
+	// write a message to the log
+	Printf(format string, obj ...interface{})
+
+	// ensure the log's buffer has been synchronized
+	Sync()
+}
 
 type MemoryLog struct {
 	Buffer bytes.Buffer
@@ -18,10 +28,19 @@ func (r *MemoryLog) Printf(format string, obj ...interface{}) {
 	statByte.Add(uint(lines) + 1)
 }
 
+func (r *MemoryLog) Sync() {
+	// memory logs don't synchronize
+}
+
 // gets the current content of the log; truncating the contents
 func (r *MemoryLog) FlushLog() []byte {
 	statFlush.Incr()
 	content := r.Buffer.Bytes()
 	r.Buffer.Reset()
 	return content
+}
+
+// create a logger for a named category
+func Logger(ctg string) Log {
+
 }
