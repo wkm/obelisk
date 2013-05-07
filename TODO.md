c46a8b215de59748c833f17ce918918742565123
Wiktor Macura <wiktor@tumblr.com>
2013-05-06 21:33:34 -0400

    [rinst] flesh out service a little

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
 xxxx * write measurements to a channel
 * trivial timestore for testing
+* copy datapoint json onto clipboard when clicking on a link
--- lib/rinst/service/service.go
+++ lib/rinst/service/service.go
@@ -6,14 +6,30 @@ import (
 	"obelisk/lib/rinst"
 )
 
-const ServiceName = "remote-instrumentation"
+const (
+	ServiceName = "remote-instrumentation"
+	FlushSize   = 500
+)
+
+type Service struct {
+	coll *rinst.Collection
+}
 
 func init() {
 	circuit.RegisterValue(&rinst.Collection{})
+	circuit.RegisterValue(&Service{})
 }
 
 // expose the instrumentation collection
 func Expose(receiver *rinst.Collection) {
 	log.Printf("exposing rinst service as %s", ServiceName)
-	circuit.Listen(ServiceName, receiver)
+	circuit.Listen(ServiceName, &Service{receiver})
+}
+
+func (s *Service) Schema() []rinst.Schema {
+	return rinst.FlushSchema(s.coll, FlushSize)
+}
+
+func (s *Service) Measure() []rinst.Measurement {
+	return rinst.FlushMeasurements(s.coll, FlushSize)
 }
