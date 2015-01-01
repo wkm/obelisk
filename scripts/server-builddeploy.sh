21385af420f99f8f56f0a0a4e1a8bce164c70350
Wiktor Macura <wkm@squareup.com>
2015-01-01 17:43:36 -0500

    Remove more remnants of Circuit

--- scripts/server-builddeploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export CIR=$HOME/Code/g0/src/obelisk/server/server.config
-4crossbuild && echo localhost | 4deploy
--- scripts/server-run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export CIR=/Users/wiktor/Code/g0/src/obelisk/server/server.config
-~/.obelisk-server/deploy/bin/server-spawn
