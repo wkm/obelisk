0ed915d2b1de213c5f1aca33fe652369d3989639
Wiktor Macura <wmacura@gmail.com>
2015-01-02 00:37:23 -0500

    As part of preparation, download all dependencies

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
+
+# Get the linting tool, updating any dependencies
 go get -u github.com/golang/lint/golint
+
+# Setup git hooks
 cp -rv githooks/ .git/hooks/
 
+# Get and update all dependencies for this project
+go get -t ./...
